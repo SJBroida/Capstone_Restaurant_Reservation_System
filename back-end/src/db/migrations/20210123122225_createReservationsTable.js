2ec41d3107f9fa735516067bd4e621999ea06824
Stephen Broida <sjbroida@gmail.com>
2021-11-17 18:08:29 -0500

    Modified createReservationsTable migration file to reflect necessary fields.

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,25 @@
+/*
+  {
+    "first_name": "Rick",
+    "last_name": "Sanchez",
+    "mobile_number": "202-555-0164",
+    "reservation_date": "2020-12-31",
+    "reservation_time": "20:00:00",
+    "people": 6,
+    "created_at": "2020-12-10T08:30:32.326Z",
+    "updated_at": "2020-12-10T08:30:32.326Z"
+  }
+*/
+
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name");
+    table.string("last_name");
+    table.string("mobile_number");
+    table.date("reservation_date");
+    table.time("reservation_time");
+    table.integer("people");
     table.timestamps(true, true);
   });
 };
